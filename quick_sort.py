66e5d53f1bef32cbe4e73b67026991f3afefe8c9
Shaun <seyyes2000@hotmail.com>
2017-02-06 10:44:28 -0500

    finish quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,20 @@
+def quick_sort(array=[4, 6, 5, 3, 7, 2, 1]):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            if x == pivot:
+                equal.append(x)
+            if x > pivot:
+                greater.append(x)
+        return quick_sort(less) + equal + quick_sort(greater)
+    else:
+        return array
+
+
+print (quick_sort())
