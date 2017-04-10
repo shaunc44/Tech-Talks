9f7fa3bece5fd460d5fae8e108e2b28205c755a0
Shaun <seyyes2000@hotmail.com>
2017-04-10 09:56:26 -0400

    updates

--- recursion_factorial.py
+++ recursion_factorial.py
@@ -9,14 +9,17 @@ def factorial(n):
 	if n == 1:
 		return 1
 	else:
-		#returns n * n-1 * (n-2) * (n-3) * ... (1)
-		#after 5 is passed into the function, factorial(n) runs inside factorial(n) which runs inside factorial(n) ...
+		#returns n * n-1 * (n-2) * (n-3) * ... * (1)
+		#after 5 is passed into the function, factorial(5) runs
+		#then factorial(4) runs inside factorial(5)
+		#and then factorial(3) runs inside factorial(4) which runs inside factorial(5)
+		#until we reach factorial(1)
 		return n * factorial(n-1)
 
 
 n = 5000
-print ("Factorial(",n,") = ", factorial(n) )
+print ("\nFactorial(",n,") = ", factorial(n) )
 
 
 stop = timeit.default_timer()
-print ("Seconds to run: ", (stop - start) )
+print ("\nSeconds to run: ", (stop - start), '\n')
